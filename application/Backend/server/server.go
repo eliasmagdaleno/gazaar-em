a14a249dc1c8007bde08fcaee5a090a3283c53ed
Elias Magdaleno <eliasrmagdaleno@gmail.com>
2025-02-22 20:23:45 -0800

    live share session to fix go back and attached everybodys pages
    Co-authored-by: ZacharyH777 <ZacharyH777@users.noreply.github.com>
    Co-authored-by: JiarongC1 <JiarongC1@users.noreply.github.com>
    Co-authored-by: WEI <wei12f8158@users.noreply.github.com>

--- application/Backend/server/server.go
+++ application/Backend/server/server.go
@@ -3,11 +3,41 @@ package server
 import (
 	"fmt"
 	"log"
+	"strings"
 	"net/http"
+	"os"
+	"path/filepath"
 
 	"github.com/aymerick/raymond"
 	"github.com/gin-gonic/gin"
 )
+
+var frontendDir = os.Getenv("FRONTEND_PATH");
+
+
+func loadFrontendFile(filelocation string) (string, error) {
+
+	var sb strings.Builder
+	sb.WriteString(frontendDir)
+	sb.WriteString(filelocation)
+
+	abspath, err := filepath.Abs(sb.String())
+
+	if err != nil {
+		return "", err
+	}
+
+
+	data, err := os.ReadFile(abspath)
+
+	// Error handling if path does not exsist
+	if err != nil {
+		return "", err
+	}
+	os.Stdout.Write(data)
+
+	return string(data), nil
+}
 
 func TestDependency() {
 	// Use raymond to prevent Go from removing it
@@ -19,11 +49,79 @@ func TestDependency() {
 func StartServer() {
 	router := gin.Default()
 
+	router.Static("/frontend", frontendDir)
+
 	router.SetTrustedProxies([]string{"192.168.0.0/24"})
 
 	router.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "Hello, Go!")
+		htmlData, err := loadFrontendFile("src/html/index.html")
+		if err != nil {
+			c.String(http.StatusInternalServerError, fmt.Sprintf("Error: %v", err))
+			return
+		}
+
+		c.Header("Content-Type", "text/html")
+		c.String(http.StatusOK, string(htmlData))
 	})
+
+	router.GET("/elias", func(c *gin.Context) {
+		htmlData, err := loadFrontendFile("src/html/member-pages/aboutme-elias.html")
+		if err != nil {
+			c.String(http.StatusInternalServerError, fmt.Sprintf("Error: %v", err))
+			return
+		}
+
+		c.Header("Content-Type", "text/html")
+		c.String(http.StatusOK, string(htmlData))
+	})
+
+	router.GET("/zachary", func(c *gin.Context) {
+		htmlData, err := loadFrontendFile("src/html/member-pages/aboutme-zachary.html")
+		if err != nil {
+			c.String(http.StatusInternalServerError, fmt.Sprintf("Error: %v", err))
+			return
+		}
+
+		c.Header("Content-Type", "text/html")
+		c.String(http.StatusOK, string(htmlData))
+	})
+
+	router.GET("/jiarong", func(c *gin.Context) {
+		htmlData, err := loadFrontendFile("src/html/member-pages/aboutme-jiarong.html")
+		if err != nil {
+			c.String(http.StatusInternalServerError, fmt.Sprintf("Error: %v", err))
+			return
+		}
+
+		c.Header("Content-Type", "text/html")
+		c.String(http.StatusOK, string(htmlData))
+	})
+
+	router.GET("/hemasri", func(c *gin.Context) {
+		htmlData, err := loadFrontendFile("src/html/member-pages/aboutme-hemasri.html")
+		if err != nil {
+			c.String(http.StatusInternalServerError, fmt.Sprintf("Error: %v", err))
+			return
+		}
+
+		c.Header("Content-Type", "text/html")
+		c.String(http.StatusOK, string(htmlData))
+	})
+
+	router.GET("/weiping", func(c *gin.Context) {
+		htmlData, err := loadFrontendFile("src/html/member-pages/aboutme-weiping.html")
+		if err != nil {
+			c.String(http.StatusInternalServerError, fmt.Sprintf("Error: %v", err))
+			return
+		}
+
+		c.Header("Content-Type", "text/html")
+		c.String(http.StatusOK, string(htmlData))
+	})
+
+
+
+	
 
 	log.Println("🚀 Server running on http://0.0.0.0:8081")
 	router.Run("0.0.0.0:8081")
