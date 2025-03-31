22403f6284d650e23f5df1bdfc87ef48f00e1484
JiarongC1 <111927017+JiarongC1@users.noreply.github.com>
2025-03-30 22:13:23 -0700

    add code to vp.go

---
+++ application/Backend/routes/vp.go
@@ -0,0 +1,22 @@
+package routes
+
+import (
+	"fmt"
+	"net/http"
+
+	"backend/server"
+
+	"github.com/gin-gonic/gin"
+)
+
+func RegisterVPRoutes(router *gin.Engine) {
+	router.GET("/vp", func(c *gin.Context) {
+		htmlData, err := server.LoadFrontendFile("src/html/vp_home.html")
+		if err != nil {
+			c.String(http.StatusInternalServerError, fmt.Sprintf("Error: %v", err))
+			return
+		}
+		c.Header("Content-Type", "text/html")
+		c.String(http.StatusOK, htmlData)
+	})
+}
